ed674bc97462e9a6796a0c4e8ac85e0a25bb19af
ParrotOS <mukto2004@gmail.com>
2025-02-21 02:49:08 +0600

    fixed seliseDetails function name

--- puppeteer.js
+++ puppeteer.js
@@ -12,7 +12,7 @@ const requestHeaders = {
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
     Referer: 'https://www.google.com/', };
 
-async function selisaDetails(page) {
+async function seliseDetails(page) {
     
     await page.goto('https://selisegroup.com/join-the-team/', { waitUntil: 'networkidle2', timeout:0 });
     
