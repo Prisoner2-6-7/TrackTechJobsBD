24eeac4f8ae72fe54150731de99f30190cdabce0
ParrotOS <mukto2004@gmail.com>
2025-02-24 03:22:11 +0600

    removed  sending err log

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.get("/", async (req, res) => {
         // .then(() => console.error('connected to db'))
         // .catch(err => console.error('couldnt connect to db:', err));
         console.error("Error fetching collection names:", err);
-        res.status(500).send("Please Refresh the page" + err);
+        res(sendStatus0).send("Please Refresh the page");
     }
 });
 
